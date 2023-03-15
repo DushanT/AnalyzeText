b8896107e4d3e8a7c8342f8b90089a3351a141e0
Dusan Tuzinsky <brooklynPD@gmail.com>
2023-03-15 20:36:51 +0100

    clean up code a turn of dealing with segments

--- code.ts
+++ code.ts
@@ -15,32 +15,42 @@ if (figma.editorType === 'figma') {
   // For storing data about library styles
   let styles: { id: string, name: string }[] = []
 
-  let searchAllPages = false
-
   // @ts-ignore
-  const isTextWithEmptyStyleId = node => node.type === 'TEXT' && node.textStyleId === ''
+  const isTextNode = node => node.type === 'TEXT'
   // @ts-ignore
-  const isTextWithStyleId = node => node.type === 'TEXT' && node.textStyleId
+  const isTextWithEmptyStyleId = node => isTextNode(node) && node.textStyleId === ''
+  // @ts-ignore
+  const isTextWithStyleId = node => isTextNode(node) && node.textStyleId
 
   // Initialize data from client storage
   const init = async () => {
-    if (figma.hasMissingFont) {
-      figma.ui.postMessage({
-        type: 'missing-fonts'
-      })
-    }
+    // if (figma.hasMissingFont) {
+    //   figma.ui.postMessage({
+    //     type: 'missing-fonts'
+    //   })
+    // }
     styles = JSON.parse(await figma.clientStorage.getAsync('styles'))
     if (styles.length > 0) {
       figma.ui.postMessage({
         type: 'loaded-styles',
         styles
       })
+      try {
+        await Promise.all(
+          // @ts-ignore
+          styles.map(style => figma.loadFontAsync(style.fontName))
+        )
+      } catch(err) {
+        console.error(`Error loading fonts: ${err}`);
+        figma.notify(`Error loading fonts: ${err}`, { error: true })
+      }
       figma.ui.resize(350, 450)
     }
   }
 
   // Save style data to client storage
   const loadStyles = () => {
+    // find all unique nested texts with style
     const textNodes = figma.currentPage.selection
       // @ts-ignore
       .reduce((acc, node) => {
@@ -58,7 +68,8 @@ if (figma.editorType === 'figma') {
         // @ts-ignore
         id: node.textStyleId,
         // @ts-ignore
-        name: figma.getStyleById(node.textStyleId)?.name
+        name: figma.getStyleById(node.textStyleId)?.name,
+        fontName: node.fontName
       }))
       figma.clientStorage.setAsync('styles', JSON.stringify(styles))
       figma.ui.postMessage({
@@ -85,28 +96,48 @@ if (figma.editorType === 'figma') {
 
   // Check if style matches node styles
   // @ts-ignore
-  const checkEquality = (style, node) => (
-    style.fontSize === node.fontSize &&
-    style.letterSpacing.unit === node.letterSpacing.unit &&
-    style.letterSpacing.value === node.letterSpacing.value &&
-    style.lineHeight.unit === node.lineHeight.unit &&
-    style.lineHeight.value === node.lineHeight.value &&
-    style.listSpacing === node.listSpacing &&
-    style.paragraphIndent === node.paragraphIndent &&
-    style.paragraphSpacing === node.paragraphSpacing &&
-    style.textCase === node.textCase &&
-    // text decoration can be broken cause of mixed styles applied to text
-    // for example text with some part manually underlined
-    (
-      style.textDecoration === node.textDecoration ||
-      (
-        typeof node.textDecoration !== 'string' &&
-        node.getStyledTextSegments(['textDecoration'])?.length > 1
-      )
-    ) &&
-    style.fontName.family === node.fontName.family &&
-    style.fontName.style === node.fontName.style
-  )
+  const checkEquality = (style, node) => {
+    return (
+      style && node &&
+      style.fontSize === node.fontSize &&
+      style.letterSpacing.unit === node.letterSpacing.unit &&
+      style.letterSpacing.value === node.letterSpacing.value &&
+      style.lineHeight.unit === node.lineHeight.unit &&
+      style.lineHeight.value === node.lineHeight.value &&
+      style.listSpacing === node.listSpacing &&
+      style.paragraphIndent === node.paragraphIndent &&
+      style.paragraphSpacing === node.paragraphSpacing &&
+      style.textCase === node.textCase &&
+      // // text decoration can be broken cause of mixed styles applied to text
+      // // for example text with some part manually underlined
+      // !!! Changing text segments always detach text style from text, this is useless
+      // !!! We should leave the manually adjusted text detached or not manually 
+      // !!! change underlines and boldes inside text nodes
+      // (
+      //   style.textDecoration === node.textDecoration || (
+      //     typeof node.textDecoration !== 'string' &&
+      //     node.getStyledTextSegments(['textDecoration'])?.length > 1
+      //   )
+      // ) &&
+      // (
+      //   (
+      //     style.fontName.family === node.fontName.family &&
+      //     style.fontName.style === node.fontName.style    
+      //   ) || 
+      //   (
+      //     typeof node.fontName !== 'string' &&
+      //     node.getStyledTextSegments(['fontName'])?.length > 1
+      //   )
+      // )
+      /** 
+       * if someone changes part of the text to underline it's not possible 
+       * to attach a text style to this node because text properties are mixed
+       */ 
+      style.textDecoration === node.textDecoration && 
+      style.fontName.family === node.fontName.family &&
+      style.fontName.style === node.fontName.style  
+    )
+  }
 
   // Find and select all nodes with equal text styles by style id 
   const findAndSelectNodes = (id: string, wrapperNode = figma.currentPage, zoom = false) => {
@@ -148,47 +179,29 @@ if (figma.editorType === 'figma') {
     findAndSelectNodes(id, figma.currentPage, true)
   }
 
-  // Find matching nodes on all pages
-  const findAllNodes = (id: string) => {
-    // @ts-ignore
-    findAndSelectNodes(id, figma.root)
-  }
-
   // Apply styles to node by style id
   const applyStyle = (id: string, node: TextNode) => {
-    const textSegments = node.getStyledTextSegments(['textDecoration'])
+    // const textSegments = node.getStyledTextSegments(['listOptions', 'textCase', 'textDecoration', 'fontName'])
     node.textStyleId = id
-    if (textSegments.length > 1) {
-      // repair mixed text decoration after style update
-      // NOT WORKING IF NOT LOADED FONTS
-      textSegments.map(segment => {
-        node.setRangeTextDecoration(segment.start, segment.end, segment.textDecoration)
-      })
-    }
+    // !!! Changing text segments always detach text style from text, this is useless 
+    // !!! because if we change text segments it will detach the style again
+    // if (textSegments.length > 1) {
+    //   textSegments.map(segment => {
+    //     node.setRangeListOptions(segment.start, segment.end, segment.listOptions)
+    //     node.setRangeTextDecoration(segment.start, segment.end, segment.textDecoration)
+    //     node.setRangeTextCase(segment.start, segment.end, segment.textCase)
+    //     node.setRangeFontName(segment.start, segment.end, segment.fontName)
+    //   })
+    // }
   }
 
   // Apply style to current page selection
   const applyStyleToSelection = (id: string) => {
-    if (figma.currentPage.selection.length > 0) {
+    const processedCount = figma.currentPage.selection.length
+    if (processedCount > 0) {
       // @ts-ignore
       figma.currentPage.selection.forEach(node => applyStyle(id, node))
-      figma.notify(`Done applying styles! Processed nodes: ${figma.currentPage.selection.length}`)
-    } else {
-      figma.notify('You have to select/find some nodes to apply styles', { error: true })
-    }
-  }
-
-  // Apply style to all pages selections
-  const applyStyleToSelectionAll = (id: string) => {
-    let count = 0
-    const selection = figma.root.children.filter(page => page.selection.length > 0)
-    if (selection.length > 0) {
-      selection.forEach(page => {
-        // @ts-ignore
-        page.selection.forEach(node => applyStyle(id, node))
-        count += page.selection.length
-      })
-      figma.notify(`Done applying styles! Processed nodes: ${count}`)
+      figma.notify(`Done applying styles! Processed nodes: ${processedCount}`)
     } else {
       figma.notify('You have to select/find some nodes to apply styles', { error: true })
     }
@@ -200,31 +213,6 @@ if (figma.editorType === 'figma') {
     figma.currentPage.selection = noStyleTexts
     figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection)
     figma.notify(`Done searching! Selected nodes: ${noStyleTexts.length}`)
-  }
-
-  // Find nodes without styles
-  const findAllNodesWithoutStyles = () => {
-    let countNodes = 0
-    let countPages = 0
-
-    figma.root.children.forEach(page => {
-      const nodes = page.findAll(node => isTextWithEmptyStyleId(node))
-      page.selection = nodes
-      countNodes += nodes.length
-      if (countNodes > 0) {
-        countPages += 1
-      }
-    })
-    figma.notify(`Done searching! Selected nodes: ${countNodes}, Pages with selections: ${countPages}`)
-  }
-
-  // Find nodes with mixed styles (only textDecoration)
-  const findNodesWithMixedStyles = () => {
-    // @ts-ignore
-    const mixedStyleTexts = figma.currentPage.findAll(node => isTextWithEmptyStyleId(node) && typeof node.textDecoration !== 'string')
-    figma.currentPage.selection = mixedStyleTexts
-    figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection)
-    figma.notify(`Done searching! Selected nodes: ${mixedStyleTexts.length}`)
   }
 
   const findMatchingStyleNodes = (styleId: string) => {
@@ -246,16 +234,6 @@ if (figma.editorType === 'figma') {
       return true
     }
     return false
-  }
-
-  // Repair nodes with mixed styles
-  const repairNodesWithMixedStyle = () => {
-    // @ts-ignore
-    const nodes = figma.currentPage.findAll(node => isTextWithEmptyStyleId(node) && typeof node.textDecoration !== 'string')
-    nodes.forEach(node => {
-      findAndApplyStyle(node)
-    })
-    figma.notify(`Done repairing! Nodes repaired: ${nodes.length}`)
   }
 
   // fix detached nodes
@@ -285,14 +263,15 @@ if (figma.editorType === 'figma') {
 
   // fix current page detached nodes
   const fixSelected = () => {
-    let countProcessed = 0
+    let countSuccessful = 0
     figma.currentPage.selection.forEach(node => {
       if (isTextWithEmptyStyleId(node) && findAndApplyStyle(node)) {
-        countProcessed += 1
+        countSuccessful += 1
       }
     })
-    if (figma.currentPage.selection.length > 0) {
-      figma.notify(`Done replacing! Nodes processed: ${figma.currentPage.selection.length}, Nodes fixed: ${countProcessed}`)
+    const countProcessed = figma.currentPage.selection.length
+    if (countProcessed > 0) {
+      figma.notify(`Done replacing! Nodes processed: ${countProcessed}, Nodes fixed: ${countSuccessful}`)
     } else {
       figma.notify('You have to select something to apply styles', { error: true })
     }
@@ -361,23 +340,11 @@ if (figma.editorType === 'figma') {
       case 'find-current-nodes':
         findCurrentNodes(msg.id);
         break;
-      case 'find-all-nodes':
-        findAllNodes(msg.id);
-        break;
       case 'apply-style':
         applyStyleToSelection(msg.id);
         break;
-      case 'apply-style-all':
-        applyStyleToSelectionAll(msg.id);
-        break;
       case 'find-no-style-nodes':
         findNodesWithoutStyles();
-        break;
-      case 'find-all-no-style-nodes':
-        findAllNodesWithoutStyles();
-        break;
-      case 'find-mixed-style-nodes':
-        findNodesWithMixedStyles();
         break;
       case 'find-matching-style-nodes':
         findMatchingStyleNodes(msg.id);
@@ -388,9 +355,6 @@ if (figma.editorType === 'figma') {
       case 'zoom-next':
         zoomTo(1);
         break;
-      case 'repair-mixed':
-        repairNodesWithMixedStyle();
-        break;
       case 'fix-selected':
         fixSelected()
         break;
@@ -399,12 +363,6 @@ if (figma.editorType === 'figma') {
         break;
       case 'fix-current-detached':
         fixCurrentDetached();
-        break;
-      case 'search-all-pages-on':
-        searchAllPages = true
-        break;
-      case 'search-all-pages-off':
-        searchAllPages = false
         break;
       case 'close':
         // Make sure to close the plugin when you're done. Otherwise the plugin will
